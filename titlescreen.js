8a1670d1cdcbfcdcf102f8124dfce4956ed92633
Jimmy Sambuo <jsambuo@gmail.com>
2016-01-30 15:49:39 -0500

    Add button to titlescreen

--- titlescreen.js
+++ titlescreen.js
@@ -1,15 +1,22 @@
 var titlescreen = function(game){}
 
 titlescreen.prototype = {
-	
+
 	preload: function() {
 		//Nothing here for now
-		
+
 	},
-	
+
 	create: function (){
 		var testSplash = this.add.sprite(0,0,"Test Title");
-		
+		this.game.add.button(this.game.world.centerX - 133.5,
+			this.game.world.centerY - 100,
+			'start_button',
+			function(){ game.state.console.log("lalala");},
+			this,
+			1,
+			0,
+			2);
 	}
-	
+
 }
