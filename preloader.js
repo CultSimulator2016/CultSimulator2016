9a8a65b374b5247f2d998f60485e00b33b74b181
Jimmy Sambuo <jsambuo@gmail.com>
2016-01-30 15:51:45 -0500

    Fix jpg to png

--- preloader.js
+++ preloader.js
@@ -4,7 +4,7 @@ preloader.prototype = {
 	preload: function(){
 		//this.game.load.image("Test Title","Honeyview_game.png");
 		this.game.load.image("Test Title","titlescreen.jpg");
-		this.game.load.image("Test Cultist","testCultist.jpg");
+		this.game.load.image("Test Cultist","testCultist.png");
 		this.game.load.spritesheet('start_button', 'assets/buttons/start_button_spritesheet.png', 267, 142);
 	},
   	create: function(){
