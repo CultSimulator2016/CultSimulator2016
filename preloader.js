9eb581ad7e182bbf757530f606ba907c259b1cad
Jimmy Sambuo <jsambuo@gmail.com>
2016-01-30 15:40:39 -0500

    Add start button

--- preloader.js
+++ preloader.js
@@ -1,10 +1,11 @@
 var preloader = function(game){}
 
 preloader.prototype = {
-	preload: function(){ 
+	preload: function(){
 		//this.game.load.image("Test Title","Honeyview_game.png");
 		this.game.load.image("Test Title","titlescreen.jpg");
 		this.game.load.image("Test Cultist","testCultist.jpg");
+		this.game.load.spritesheet('start_button', 'assets/buttons/start_button_spritesheet.png', 267, 142);
 	},
   	create: function(){
 		this.game.state.start("Title Screen");
