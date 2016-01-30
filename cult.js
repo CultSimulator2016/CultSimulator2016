d663b5bf543b2068ab7dcc6383f02bdf896638bd
HL <foreverlooped@gmail.com>
2016-01-30 16:24:31 -0500

    added buttons to cult.js

--- cult.js
+++ cult.js
@@ -9,5 +9,31 @@ cult.prototype = {
 		//this.game.state.start("Title Screen");
 		var testSplash = this.add.sprite(0,0,"Test Cultist");
 
+		this.game.add.button(this.game.world.centerX - 200,
+				this.game.world.centerY + 50,
+				'temp_button',
+				function(){ console.log("recruit button clicked");},
+				this,
+				1,
+				0,
+				2);
+				
+		this.game.add.button(this.game.world.centerX - 200,
+				this.game.world.centerY + 80,
+				'temp_button',
+				function(){ console.log("shop button clicked");},
+				this,
+				1,
+				0,
+				2);
+				
+		this.game.add.button(this.game.world.centerX - 200,
+				this.game.world.centerY + 120,
+				'temp_button',
+				function(){ console.log("main button clicked");},
+				this,
+				1,
+				0,
+				2);
 	}
 }
