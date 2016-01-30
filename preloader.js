75ba28a3f8f586357a04a4e297700be337d95d67
Bob Huang <joleifake@gmail.com>
2016-01-30 15:33:36 -0500

    Added a test cultist

--- preloader.js
+++ preloader.js
@@ -3,6 +3,8 @@ var preloader = function(game){}
 preloader.prototype = {
 	preload: function(){ 
 		//this.game.load.image("Test Title","Honeyview_game.png");
+		this.game.load.image("Test Title","titlescreen.jpg");
+		this.game.load.image("Test Cultist","testCultist.jpg");
 	},
   	create: function(){
 		this.game.state.start("Title Screen");
