2f683837e94993976d4d7ce613ad6ea71bbee15d
Bob Huang <joleifake@gmail.com>
2016-01-30 14:07:36 -0500

    Got the modular individual javascript files working. Index + Colin_scenes now loads the Rough draft game image.

---
+++ titlescreen.js
@@ -0,0 +1,15 @@
+var titlescreen = function(game){}
+
+titlescreen.prototype = {
+	
+	preload: function() {
+		//Nothing here for now
+		
+	},
+	
+	create: function (){
+		var testSplash = this.add.sprite(0,0,"Test Title");
+		
+	}
+	
+}
