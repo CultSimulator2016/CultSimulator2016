7f2835d4222f294846ecb048a47b033bbd124002
Jimmy Sambuo <jsambuo@gmail.com>
2016-01-31 14:21:04 -0500

    Fix history and other tweaks to engine

--- src/engine.js
+++ src/engine.js
@@ -4,10 +4,13 @@ function Engine() {
           { id: 2, text: "Dance Naked", cost: 150, image: "card_dance_naked" },
           { id: 3, text: "Eat Cat Food", cost: 200, image: "card_eat_cat_food" }
   ];
-  this.shopAvailableCards = this.cardsList;
+  this.shopAvailableCards = JSON.parse(JSON.stringify(this.cardsList)); // Deep copy hack
   this.playerAvailableCards = [];
   this.money = 500;
   this.currentDay = 1;
+  this.overallLoyalty = 10;
+  this.followersCount = 5;
+  this.ritualHistory = [];
 };
 Engine.prototype.getDay = function() {
   return this.currentDay;
@@ -16,13 +19,14 @@ Engine.prototype.getMoney = function() {
   return this.money;
 };
 Engine.prototype.getOverallLoyalty = function() {
-  return 42;
+  return this.overallLoyalty;
 };
 Engine.prototype.getFollowersCount = function() {
-  return 50;
+  return this.followersCount;
 };
 Engine.prototype.recruit = function() {
-  console.log("Recruiting..");
+  this.followersCount += 5;
+  this.money -= 50;
 };
 Engine.prototype.getCardsShop = function() {
   return this.shopAvailableCards;
@@ -31,19 +35,20 @@ Engine.prototype.getCardsRitual = function() {
   return this.playerAvailableCards;
 };
 Engine.prototype.buyCard = function(ritualId) {
-  console.log("Buying card with ritualId: " + ritualId);
   var card = this.cardsList.filter(function(item){ return item.id === ritualId; })[0];
   this.money -= card.cost;
   this.playerAvailableCards.push(card);
   this.shopAvailableCards.splice(this.shopAvailableCards.indexOf(card), 1);
 };
 Engine.prototype.makeRitual = function(ritualId) {
-  console.log("Ritual with ritualId: " + ritualId);
+  var card = this.cardsList.filter(function(item){ return item.id === ritualId; })[0];
+  console.log(card, this.cardsList, ritualId, this.cardsList.filter(function(item){ return item.id === ritualId; }));
   this.currentDay += 1;
+  this.ritualHistory.push(card.text);
+  this.overallLoyalty += 5;
   return ["Loyalty Increased!",
           "2 followers left your cult.."];
 };
 Engine.prototype.getHistory = function() {
-  return ["Kill chickens",
-          "Dance naked"];
+  return this.ritualHistory;
 };
