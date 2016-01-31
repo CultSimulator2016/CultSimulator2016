076c36e8b5513888683039bb6f0c96c8cda1a8c0
Jimmy Sambuo <jsambuo@gmail.com>
2016-01-31 13:13:36 -0500

    Preload cards and rituals screen

--- src/states/preloader.js
+++ src/states/preloader.js
@@ -6,7 +6,7 @@ preloader.prototype = {
 		//this.game.load.image("Test Title","Honeyview_game.png");
 		this.game.load.image("Title","assets/screens/titlescreen2.jpg");
 		this.game.load.image("Test Cultist","testCultist.png");
-		this.game.load.image("Test Ritual","assets/screens/deckscreen.jpg");
+		this.game.load.image("Test Ritual","assets/screens/ritualscreen.jpg");
 		this.game.load.image("Shop","assets/screens/shopscreen2.jpg");
 		this.game.load.image("Cult","assets/screens/cultinfoscreen.jpg");
 		this.game.load.image("Test Recruitment","assets/screens/recruitscreen.jpg");
@@ -17,6 +17,9 @@ preloader.prototype = {
 		this.game.load.image("Mainscreen Background","assets/screens/mainscreen_clean.jpg");
 		this.game.load.image("Devil","assets/UI/devil.png");
 		this.game.load.image("Dialogue box","assets/ui/dialogbox.png");
+		this.game.load.image("card_kill_chicken","assets/UI/card_kill_chicken.png");
+		this.game.load.image("card_eat_cat_food","assets/UI/card_eat_cat_food.png");
+		this.game.load.image("card_dance_naked","assets/UI/card_dance_naked.png");
 		this.game.load.spritesheet('start_button', 'assets/buttons/start_button_spritesheet.png', 267, 142);
 		this.game.load.spritesheet('temp_button', 'buttonspritesheet.png', 200, 50);
 		this.game.load.spritesheet('cult_button', 'assets/buttons/cult_info.png', 214,70);
